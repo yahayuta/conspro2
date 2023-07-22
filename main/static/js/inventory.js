e85745c8c04916e1fdcc4f56d6c0df9b01f60465
やはゆた <13937582+yahayuta@users.noreply.github.com>
2023-07-22 22:30:30 +0900

    金額計算ロジック

--- main/static/js/inventory.js
+++ main/static/js/inventory.js
@@ -37,3 +37,13 @@ function downloadProformaInvoice() {
     document.forms[1].method = "POST";
 	document.forms[1].submit();
 }
+
+/**
+ * 金額計算
+ * @param {*} obj 
+ */
+function autoSum(obj) {
+	obj.order_cost_price.value = Number(obj.order_price.value) + Number(obj.order_trans_cost.value) + Number(obj.parts_cost.value) + Number(obj.maintenance_cost.value) + Number(obj.order_out_order_cost.value);
+	obj.sell_cost_price.value = Number(obj.sell_trance_cost.value) + Number(obj.ship_cost.value) + Number(obj.sell_out_order_cost.value) + Number(obj.ins_cost.value) + Number(obj.freight_cost.value) + Number(obj.order_cost_price.value);
+	obj.profit.value = Number(obj.sell_price.value) - Number(obj.sell_cost_price.value);
+}
