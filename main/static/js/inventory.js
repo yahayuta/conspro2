a6d008fba75a0e313b33d16d844c11b4b9e33d5e
やはゆた <13937582+yahayuta@users.noreply.github.com>
2024-06-18 08:55:46 +0900

    在庫注文明細計算処理

--- main/static/js/inventory.js
+++ main/static/js/inventory.js
@@ -12,6 +12,13 @@ $(document).ready(function() {
 		language: "ja" //日本語化
 	});
 
+    for (let i = 0; i < 17; i++) {
+        $('#id_inventory_id-' + i + '-is_out').blur(calc_invoice);
+        $('#id_inventory_id-' + i + '-count').blur(calc_invoice);
+        $('#id_inventory_id-' + i + '-price').blur(calc_invoice);
+    }
+    calc_invoice();
+
 	const inputElements = document.querySelectorAll('input[type="text"], input[type="number"], input[type="date"]');
 	inputElements.forEach(inputElement => {
 		inputElement.classList.add('form-control');
@@ -53,4 +60,17 @@ function autoSum(obj) {
 	obj.order_cost_price.value = Number(obj.order_price.value) + Number(obj.order_trans_cost.value) + Number(obj.parts_cost.value) + Number(obj.maintenance_cost.value) + Number(obj.order_out_order_cost.value);
 	obj.sell_cost_price.value = Number(obj.sell_trance_cost.value) + Number(obj.ship_cost.value) + Number(obj.sell_out_order_cost.value) + Number(obj.ins_cost.value) + Number(obj.freight_cost.value) + Number(obj.order_cost_price.value);
 	obj.profit.value = Number(obj.sell_price.value) - Number(obj.sell_cost_price.value);
+}
+
+/**
+ * 請求金額計算する
+ */
+function calc_invoice() {
+    // 明細行の金額算出と足しこみ
+    for (let i = 0; i < 17; i++) {
+        // 請求書出力対象かどうかチェック
+        if (!$('#id_inventory_id-' + i + '-is_out').prop("checked")) continue;
+        const row_total = Number($('#id_inventory_id-' + i + '-count').val()) * Number($('#id_inventory_id-' + i + '-price').val());
+        $('#id_inventory_id-' + i + '-total').val(row_total);
+    }
 }
